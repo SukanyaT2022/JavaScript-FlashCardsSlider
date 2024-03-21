67cc8faa9e62b2b560146d1d5e062a32f02d03b3
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-03-21 16:08:51 -0400

    add

---
+++ practice.js
@@ -0,0 +1,24 @@
+var arrAllQandA = [
+
+["Question1", "optionOne", "optionTwo", "optionThree","optionFour", "optionOne"],
+
+["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionTwo"],
+
+["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionThree"],
+
+["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionFour"],
+
+["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionFour"],
+
+["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionThree"]
+]
+
+//two dimension array
+//while loop use when when we donot know how many question
+//for loop we know exactly how many questions
+function load(){
+
+    for(){
+
+    }
+}
