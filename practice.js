03053093212d8386af59c7f37a6d369e9955ef39
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-03-22 14:30:19 -0400

    add

--- practice.js
+++ practice.js
@@ -4,13 +4,13 @@ var arrAllQandA = [
 
 ["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionTwo"],
 
-["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionThree"],
+["Question3", "optionOne", "optionTwo", "optionThree","optionFour", "optionThree"],
 
-["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionFour"],
+["Question4", "optionOne", "optionTwo", "optionThree","optionFour", "optionFour"],
 
-["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionFour"],
+["Question5", "optionOne", "optionTwo", "optionThree","optionFour", "optionFour"],
 
-["Question2", "optionOne", "optionTwo", "optionThree","optionFour", "optionThree"]
+["Question6", "optionOne", "optionTwo", "optionThree","optionFour", "optionThree"]
 ]
 
 //two dimension array
@@ -18,7 +18,76 @@ var arrAllQandA = [
 //for loop we know exactly how many questions
 function load(){
 
-    for(){
+    for(i=0;i<arrAllQandA.length;i++){
+var storeLabel = document.createElement('H2')// crate label question
+var textNote = document.createTextNode(arrAllQandA[i][0])// text in label
+storeLabel.appendChild(textNote)//we put textNote in label by appenchild()
+document.getElementById('showRadioButton').appendChild(storeLabel)//STORELABEL LINE 22
+//1
+//type is attribute of the tag--crate radio button
+var storeRadioButton = document.createElement('input')
+storeRadioButton.setAttribute('type', 'radio')
+storeRadioButton.setAttribute('class','blockE4RadioB')
+
+
+var name = 'question' + i
+storeRadioButton.setAttribute('name', name)
+storeRadioButton.setAttribute('value', arrAllQandA[i][1])//i position number
+document.getElementById('showRadioButton').appendChild(storeRadioButton)
+
+//crate text next to radio button
+var storeTextNoteNext2RadioButton = document.createTextNode(arrAllQandA[i][1])
+document.getElementById('showRadioButton').appendChild(storeTextNoteNext2RadioButton)
+//end input radio button
+//2
+//type is attribute of the tag--crate radio button
+var storeRadioButton = document.createElement('input')
+storeRadioButton.setAttribute('type', 'radio')
+storeRadioButton.setAttribute('class','blockE4RadioB')
+
+
+storeRadioButton.setAttribute('name', name)
+storeRadioButton.setAttribute('value', arrAllQandA[i][2])//i position number
+document.getElementById('showRadioButton').appendChild(storeRadioButton)
+
+//crate text next to radio button
+var storeTextNoteNext2RadioButton = document.createTextNode(arrAllQandA[i][2])
+document.getElementById('showRadioButton').appendChild(storeTextNoteNext2RadioButton)
+//end input radio button
+//3
+//type is attribute of the tag--crate radio button
+var storeRadioButton = document.createElement('input')
+storeRadioButton.setAttribute('type', 'radio')
+storeRadioButton.setAttribute('class','blockE4RadioB')
+
+
+storeRadioButton.setAttribute('name', name)
+storeRadioButton.setAttribute('value', arrAllQandA[i][3])//i position number
+document.getElementById('showRadioButton').appendChild(storeRadioButton)
+
+//crate text next to radio button
+var storeTextNoteNext2RadioButton = document.createTextNode(arrAllQandA[i][3])
+document.getElementById('showRadioButton').appendChild(storeTextNoteNext2RadioButton)
+//end input radio button
+
+//4
+//type is attribute of the tag--crate radio button
+var storeRadioButton = document.createElement('input')
+storeRadioButton.setAttribute('type', 'radio')
+storeRadioButton.setAttribute('class','blockE4RadioB')
+
+var name = 'question' + i
+storeRadioButton.setAttribute('name', name)
+storeRadioButton.setAttribute('value', arrAllQandA[i][4])//i position number
+document.getElementById('showRadioButton').appendChild(storeRadioButton)
+
+//crate text next to radio button
+var storeTextNoteNext2RadioButton = document.createTextNode(arrAllQandA[i][4])
+document.getElementById('showRadioButton').appendChild(storeTextNoteNext2RadioButton)
+//end input radio button
+
+
 
     }
+
 }
